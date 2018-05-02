304c86c0d30b60c5d4a64db7acfb4c109bc74db2
Eugene Petrenko <eugene.petrenko@gmail.com>
2018-05-02 12:51:54 +0200

    refactor reading of JSON to a dedicated class

--- src/csharp/BelgianBeers/BelgianBeers/Repositories/BeersRepository.cs
+++ src/csharp/BelgianBeers/BelgianBeers/Repositories/BeersRepository.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using BelgianBeers.Models;
 using JetBrains.Annotations;
+using Microsoft.VisualStudio.TestPlatform.Common.DataCollection;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -14,54 +15,40 @@ namespace BelgianBeers.Repositories
     {
         private readonly HashSet<Brewery> _breweries = new HashSet<Brewery>();
         private readonly HashSet<Beer> _beers = new HashSet<Beer>();
-        
-        public static async Task<BeersRepository> FromFile([PathReference] string file)
-        {            
-            if (!File.Exists(file))
-            {
-                throw new FileNotFoundException("Data file not found.", file);
-            }
-            
-            var repository = new BeersRepository();
-            
-            using (var reader = new JsonTextReader(new StreamReader(File.OpenRead(file))))
-            {
-                while (await reader.ReadAsync())
-                {
-                    if (reader.TokenType == JsonToken.StartObject)
-                    {
-                        // Load data from the stream
-                        var beerData = JObject.Load(reader);
 
-                        var breweryName = beerData.Value<string>("brewery");
-                        var beerName = beerData.Value<string>("name");
-                        var rating = beerData.Value<double>("rating");
-                        var votes = beerData.Value<double>("votes");
-                        
-                        #region Bad approach - No nulls in data
-                        
-                        // Store the brewery
-                        // TODO DEMO: This get/add pattern is stupid! Add GetHashCode to Brewery() and just call .Add on the underlying hashset
-                        var brewery = repository.GetBrewery(breweryName);
-                        if (brewery == null)
-                        {
-                            brewery = new Brewery(breweryName);
-                            repository.AddBrewery(brewery);
-                        }
-                        
-                        // Store the beer
-                        // TODO DEMO: This get/add is needed to ensure no duplicates, however we could again do a GetHashCode() instead
-                        var beer = repository.GetBeer(beerName);
-                        if (beer == null)
-                        {
-                            beer = new Beer(beerName, brewery, rating, votes);
-                            repository.AddBeer(beer);
-                        }
-                        
-                        #endregion
-                        
-                        #region Bad approach - Nulls in data
-                        
+        public static async Task<BeersRepository> FromFile([PathReference] string file)
+        {
+            var repository = new BeersRepository();
+
+            BeersStream.FromFile(file, beer =>
+            {
+                var breweryName = beer.Brewery.Name;
+                var beerName = beer.Name;
+                var rating = beer.Rating;
+                var votes = beer.Votes;
+                
+                #region Bad approach - No nulls in data
+
+                // Store the brewery
+                // TODO DEMO: This get/add pattern is stupid! Add GetHashCode to Brewery() and just call .Add on the underlying hashset
+                var brewery = repository.GetBrewery(breweryName);
+                if (brewery == null)
+                {
+                    brewery = new Brewery(breweryName);
+                    repository.AddBrewery(brewery);
+                }
+
+                // Store the beer
+                // TODO DEMO: This get/add is needed to ensure no duplicates, however we could again do a GetHashCode() instead
+                if (repository.GetBeer(beer.Name) == null)
+                {
+                    repository.AddBeer(beer);
+                }
+
+                #endregion
+
+                #region Bad approach - Nulls in data
+
 //                        // Store the brewery
 //                        // TODO DEMO: This get/add pattern is stupid! Add GetHashCode to Brewery() and just call .Add on the underlying hashset
 //                        // TODO DEMO: Null checks here are ugly, quick. Does Kotlin have anything better?
@@ -84,11 +71,11 @@ namespace BelgianBeers.Repositories
 //                            beer = new Beer(beerName, brewery, rating, votes);
 //                            repository.AddBeer(beer);
 //                        }
-                        
-                        #endregion
-                        
-                        #region Good approach
-                        
+
+                #endregion
+
+                #region Good approach
+
 //                        // Store the brewery
 //                        // TODO DEMO: Null checks here are ugly, quick. Does Kotlin have anything better?
 //                        var brewery = !string.IsNullOrEmpty(breweryName)
@@ -99,12 +86,11 @@ namespace BelgianBeers.Repositories
 //                        // Store the beer
 //                        var beer = new Beer(beerName, brewery, rating, votes);
 //                        repository.AddBeer(beer);
-                        
-                        #endregion
-                    }
-                }
-            }
 
+                #endregion
+            });
+            
+            
             return repository;
         }
 
@@ -146,7 +132,7 @@ namespace BelgianBeers.Repositories
             {
                 beer = new TripelBeer(beer.Name, beer.Brewery, beer.Rating, beer.Votes);
             }
-            
+
             _beers.Add(beer);
         }
     }
---
+++ src/csharp/BelgianBeers/BelgianBeers/Repositories/BeersStream.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using BelgianBeers.Models;
+using JetBrains.Annotations;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace BelgianBeers.Repositories
+{
+    public static class BeersStream
+    {
+        public static async void FromFile([PathReference] string file, Action<Beer> callback)
+        {
+            if (!File.Exists(file))
+            {
+                throw new FileNotFoundException("Data file not found.", file);
+            }
+
+            using (var reader = new JsonTextReader(new StreamReader(File.OpenRead(file))))
+            {
+                while (await reader.ReadAsync())
+                {
+                    if (reader.TokenType == JsonToken.StartObject)
+                    {
+                        // Load data from the stream
+                        var beerData = JObject.Load(reader);
+
+                        var breweryName = beerData.Value<string>("brewery");
+                        var beerName = beerData.Value<string>("name");
+                        var rating = beerData.Value<double>("rating");
+                        var votes = beerData.Value<double>("votes");
+
+                        var beer = new Beer(beerName, new Brewery(breweryName), rating, votes);
+                        callback(beer);
+                    }
+                }
+            }
+        }
+    }
+}
