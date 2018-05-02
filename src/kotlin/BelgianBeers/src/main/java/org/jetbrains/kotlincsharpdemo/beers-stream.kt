82dca380b2876ec34aae3006f1742c02e5108a9a
Eugene Petrenko <eugene.petrenko@gmail.com>
2018-05-02 14:00:38 +0200

    rework stream API

--- src/kotlin/BelgianBeers/src/main/java/org/jetbrains/kotlincsharpdemo/beers-stream.kt
+++ src/kotlin/BelgianBeers/src/main/java/org/jetbrains/kotlincsharpdemo/beers-stream.kt
@@ -1,34 +1,45 @@
 package org.jetbrains.kotlincsharpdemo
 
+import com.fasterxml.jackson.core.JsonParser
 import com.fasterxml.jackson.core.JsonToken
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.node.ObjectNode
 import java.io.File
 import kotlin.coroutines.experimental.buildIterator
 
-fun streamBeersFromFile(file: File) = buildIterator {
-  if (!file.isFile) {
-    throw Error("Data file not found. $file");
-  }
+data class BeerItem(
+        val name: String?,
+        val brewery: String?,
+        val rating: Double?,
+        val votes: Double?
+)
 
-  val om = ObjectMapper()
+object BeersStream {
 
-  using(om.factory.createParser(file)) { parser ->
-    if (parser.nextToken() != JsonToken.START_ARRAY) throw error("Array was expected in JSON data")
+  fun fromString(text: String) = ObjectMapper().factory.createParser(text).streamData()
 
-    while (parser.nextToken() == JsonToken.START_OBJECT) {
-      val beerData = parser.readValueAsTree<ObjectNode>()
+  fun fromFile(file: File): Sequence<BeerItem> = ObjectMapper().factory.createParser(file).streamData()
 
-      val breweryName = beerData["brewery"]?.asText()
-      val beerName = beerData["name"].asText()
-      val rating = beerData["rating"].asDouble()
-      val votes = beerData["votes"].asDouble()
 
-      yield(Beer(
-              Name = beerName,
-              Brewery = breweryName?.let { Brewery(it) },
-              Rating = rating,
-              Votes = votes))
+  fun JsonParser.streamData() = buildIterator {
+    use { parser ->
+      if (parser.nextToken() != JsonToken.START_ARRAY) throw error("Array was expected in JSON data")
+
+      while (parser.nextToken() == JsonToken.START_OBJECT) {
+        val beerData = parser.readValueAsTree<ObjectNode>()
+
+        val breweryName = beerData["brewery"]?.asText()
+        val beerName = beerData["name"]?.asText()
+        val rating = beerData["rating"]?.asDouble()
+        val votes = beerData["votes"]?.asDouble()
+
+        yield(BeerItem(
+                name = beerName,
+                brewery = breweryName,
+                rating = rating,
+                votes = votes))
+      }
     }
-  }
+  }.asSequence()
+
 }
