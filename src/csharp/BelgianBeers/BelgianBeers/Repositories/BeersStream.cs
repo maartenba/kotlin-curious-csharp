81dae13c32bd119907e83d833f90ac9c518ebcc2
Eugene Petrenko <eugene.petrenko@gmail.com>
2018-05-03 14:58:12 +0200

    remove unused class

--- src/csharp/BelgianBeers/BelgianBeers/Repositories/BeersStream.cs
+++ src/csharp/BelgianBeers/BelgianBeers/Repositories/BeersStream.cs
@@ -35,38 +35,5 @@ namespace BelgianBeers.Repositories
                 }
             }
         }
-        
-        public static IEnumerable<Beer> FromFileAsBeers([PathReference] string file)
-        {
-            if (!File.Exists(file))
-            {
-                throw new FileNotFoundException("Data file not found.", file);
-            }
-
-            using (var reader = new JsonTextReader(new StreamReader(File.OpenRead(file))))
-            {
-                while (reader.Read())
-                {
-                    if (reader.TokenType == JsonToken.StartObject)
-                    {
-                        // Load data from the stream
-                        var beerData = JObject.Load(reader);
-
-                        var breweryName = beerData.Value<string>("brewery");
-                        var beerName = beerData.Value<string>("name");
-                        var rating = beerData.Value<double>("rating");
-                        var votes = beerData.Value<double>("votes");
-
-                        yield return new Beer(
-                            beerName, 
-                            !string.IsNullOrEmpty(breweryName) 
-                                ? new Brewery(breweryName)
-                                : null, 
-                            rating,
-                            votes);
-                    }
-                }
-            }
-        }
     }
 }
