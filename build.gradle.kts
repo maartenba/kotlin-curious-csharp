d445bcbeaea63327577e25621ad65f64f2cd57b3
Eugene Petrenko <eugene.petrenko@gmail.com>
2018-05-04 10:27:21 +0200

    apply kotlin-frontend plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,13 @@ buildscript {
 
   repositories {
     jcenter()
+    mavenCentral()
+    maven(url = "https://dl.bintray.com/kotlin/kotlin-eap")
   }
 
   dependencies {
     classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.2.41")
+    classpath("org.jetbrains.kotlin:kotlin-frontend-plugin:0.0.30")
   }
 }
 
--- src/kotlin/BelgianBeers/js/build.gradle.kts
+++ src/kotlin/BelgianBeers/js/build.gradle.kts
@@ -1,15 +1,19 @@
 import org.jetbrains.kotlin.gradle.dsl.Coroutines
+import org.jetbrains.kotlin.gradle.frontend.KotlinFrontendExtension
+import org.jetbrains.kotlin.gradle.frontend.npm.NpmExtension
+import org.jetbrains.kotlin.gradle.frontend.util.frontendExtension
+import org.jetbrains.kotlin.gradle.frontend.webpack.WebPackExtension
 import org.jetbrains.kotlin.gradle.tasks.Kotlin2JsCompile
 
 plugins {
   base
   id("kotlin-platform-js")
+  id("org.jetbrains.kotlin.frontend")
 }
 
 
 dependencies {
-  add("expectedBy", project(":src:kotlin:BelgianBeers:common"))
-
+  expectedBy(project(":src:kotlin:BelgianBeers:common"))
 
   compile(kotlin("stdlib-js"))
   testCompile(kotlin("test-js"))
@@ -18,5 +22,18 @@ dependencies {
 
 kotlin {
   experimental.coroutines = Coroutines.ENABLE
+}
+
+tasks.withType<Kotlin2JsCompile> {
+  kotlinOptions.metaInfo = true
+  kotlinOptions.outputFile = "${project.buildDir.path}/js/${project.name}-${name}.js"
+  kotlinOptions.sourceMap = true
+  kotlinOptions.moduleKind = "commonjs"
+  kotlinOptions.main = "call"
+}
+
+extensions.getByType(NpmExtension::class.java).apply {
+  dependency("qunit")
+  devDependency("karma")
 }
 
